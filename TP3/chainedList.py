4eb4272d05cef90b0ab6db7ffc7067a92eb9a71c
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-01-26 09:52:05 +0100

    essai infructueux

--- TP3/chainedList.py
+++ TP3/chainedList.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 # coding: utf-8
-class ChainedList:
+from node import Node
+
+class ChainedList(Node):
     """
     Chained list Object
     Parameters
@@ -8,23 +11,62 @@ class ChainedList:
         list that we want to transfert in a chained list of Node object
     """
     def __init__(self, nodes):
-        self.first_node = None
-
-    def insert_node_after(self, data, new_node):
+        self.first_node = nodes[0] #list empty
+        self.node = list(nodes)
+        
+    def __str__(self):
+        liste = []
+        for elt in self.node:
+            liste.append(elt)
+        return str(liste)
+    
+    def empty_list(self):
+        if self.first_node is None:
+            print("This list is empty")
+        else:
+            print("this list isn't empty")
+    
+    def list_size(self):
+        element = self.first_node
+        length = 0
+        while element is not None:
+            length += 1
+            element = element.link
+            return length
+        
+    def add_start(self, first_element):
+        first_element.link = self.first_node
+        self.first_node = first_element
+        self.node.append(first_element)
+        print(self.node)
+   
+    def add_end(self, end_element):
+        element = self.first_node
+        while element.link is not None:
+            element = element.link
+        element.link = end_element
+    
+    def insert_node_after(self, before_element, new_element):
         """
-        insert a new node after the node with the value == data
+        insert a new node after the node with the value == before_element
         Parameters
         ----------
-        data : searched data
-        new_node : node to insert
+        before_element : searched data
+        new_element : node to insert
         """
-        pass
-
-    def delete_node(self, data):
+        new_element.link = before_element.link
+        before_element.link = new_element
+        return self
+      
+    def delete_node(self, element, before_element):
         """
         delete all node(s) value == data
         Parameters
         ----------
         data : searched data to delete
         """
-        pass
+        if element == self.first_node:
+            self.first_node = element.link
+        elif element == before_element.link:
+            before_element.link = element.link
+            
--- TP3/node.py
+++ TP3/node.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # coding: utf-8
 class Node:
     def __init__(self, param_data):
