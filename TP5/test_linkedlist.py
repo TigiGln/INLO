01f952477237bd02722d6310864e9f736d19b435
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-02-09 09:13:33 +0100

    petite modif

--- TP5/test_linkedlist.py
+++ TP5/test_linkedlist.py
@@ -21,40 +21,39 @@ class TestLinkList(unittest.TestCase):
         method called for performing the tests, Creation
         of the test linked list
         """
-        self.liste = linkedList.LinkedList(["g", "c", "f"])
+        self.liste1 = linkedList.LinkedList()
+        self.liste2 = linkedList.LinkedList(["g", "c", "f"])
     def test_list_empty(self):
         """
         Test allowing the verification of an empty list
         and the return Raise Exception
         """
-        if str(self.liste) == "[]":
-            with self.assertRaises(Exception, msg="List is empty"):
-                self.liste.remove_node("x")
+        with self.assertRaises(Exception, msg="List is empty"):
+            self.liste1.remove_node("x")
+        print("No element can be deleted in this list since it is empty")
     def test_list_no_empty_add_value(self):
         """
         Test if a linked list is not empty after adding a node
         """
-        #list_init = str(self.liste)
-        self.liste.add_first(linkedList.Node("v"))
-        #result= str(self.liste)
-        self.assertFalse(self.liste.list_is_empty())
+        self.liste1.add_first(linkedList.Node("v"))
+        self.assertFalse(self.liste1.list_is_empty())
     def test_no_modif(self):
         """
         Test if the linked list is not modified when simultaneously adding
         and removing the same element
         """
-        list_init = str(self.liste)
-        self.liste.add_first(linkedList.Node("v"))
-        self.liste.remove_node("v")
-        result = str(self.liste)
+        list_init = str(self.liste2)
+        self.liste2.add_first(linkedList.Node("v"))
+        self.liste2.remove_node("v")
+        result = str(self.liste2)
         self.assertEqual(result, list_init)
     def test_first_element(self):
         """
         Test if the first element is the one added at the beginning
         of our linked list
         """
-        self.liste.add_first(linkedList.Node("e"))
-        result = list(self.liste)
+        self.liste2.add_first(linkedList.Node("e"))
+        result = list(self.liste2)
         self.assertEqual(str(result[0]), "e")
 if __name__ == '__main__':
     unittest.main()
